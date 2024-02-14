1a9b0543f49a6fbd60dc0e4838107513cb5bc7e9
Eseteselase <esete.s.berhanu@gmail.com>
2024-02-14 11:25:18 +0300

    4

---
+++ 0x00-challenge/4-delete_dnodeint/main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+int index_to_delete = 0;
+{
+	dlistint_t *head;
+
+	head = NULL;
+	add_dnodeint_end(&head, 0);
+	add_dnodeint_end(&head, 1);
+	add_dnodeint_end(&head, 2);
+	add_dnodeint_end(&head, 3);
+	add_dnodeint_end(&head, 4);
+	add_dnodeint_end(&head, 98);
+	add_dnodeint_end(&head, 402);
+	add_dnodeint_end(&head, 1024);
+	print_dlistint(head);
+	while (head != NULL)
+	{
+		delete_dnodeint_at_index(&head, index_to_delete);
+		print_dlistint(head);
+		printf("-----------------\n");
+		index_to_delete = 0;
+	}
+	free_dlistint(head);
+	return (0);
+}
